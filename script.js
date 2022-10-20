d17ac1c8d4c14f38431a65322af6fe8651f02768
ChloeFoxLisa <55240605+ChloeFoxLisa@users.noreply.github.com>
2022-10-20 17:48:55 +0300

    Slider

---
+++ script.js
@@ -0,0 +1,34 @@
+const upBtn = document.querySelector('.up-button');
+const downBtn = document.querySelector('.down-button');
+const sidebar = document.querySelector('.sidebar');
+const container = document.querySelector('.container');
+const mainSlide = document.querySelector('.main-slide');
+const slidesCount = mainSlide.querySelectorAll('div').length;
+
+const height = container.clientHeight;
+
+let activeSlideIndex = 0;
+
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh`;
+
+upBtn.addEventListener('click', () => {
+    changeSlide('up');
+})
+
+downBtn.addEventListener('click', () => {
+    changeSlide('down');
+})
+
+function changeSlide(direction) {
+    if (direction === 'up') {
+        activeSlideIndex++;
+        activeSlideIndex === slidesCount ? activeSlideIndex = 0 : null;
+    } else if (direction === 'down') {
+        activeSlideIndex--;
+        activeSlideIndex < 0 ? activeSlideIndex = slidesCount - 1 : null;
+    }
+
+    mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`;
+
+    sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`;
+}
